f408e6c88cff955e7934499a7c3a7b8e49cda1fa
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-01-02 22:09:16 -0700

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,27 @@ class App extends Component {
   render() {
       return (
          <div>
-            <nav className="main-nav">
-               <h3>MY SITE</h3>
-               <div>
-                  <Search />
-                  <Link to="/">Home</Link>
-                  <Link to="/games">Games</Link>
-                  <Link to="/books">Books</Link>
-                  <Link to="/posters">Posters</Link>
-                  <Link to="/signin">Sign In</Link> {/* The signin link will need to change to the user link after logging in */}
-               </div>
-            </nav>
-            { router }
+            <div className= "header">
+               <nav className="header-nav">
+                  <h2 className="header-title">MY SITE</h2>
+                  <div>
+                     <Search />
+                     <Link to="/">Home</Link>
+                     <Link to="/games">Games</Link>
+                     <Link to="/books">Books</Link>
+                     <Link to="/posters">Posters</Link>
+                     <Link to="/signin">Sign In</Link> {/* The signin link will need to change to the user link after logging in */}
+                  </div>
+               </nav>
+            </div>
+
+            <div className="main">
+               { router }
+            </div>
+            
+            <div className="footer">
+               <p>&copy; 2018 My Site</p>
+            </div>
          </div>
       );
   }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,10 +1,17 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SignIn extends Component {
    render () {
       return (
          <div>
             <h3>The SignIn Component</h3>
+            <div>
+                <input className="login-input" placeholder="username" />
+                <input className="login-input" placeholder="password" />
+                <button className="signin-btn">Sign In</button>
+                <Link to="/register"><button className="register-btn">Register</button></Link>
+            </div>
          </div>
       )
    }
