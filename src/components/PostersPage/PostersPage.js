922625f6b15a486ba09a77377b16ba56d16f9f30
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-01-28 03:34:07 -0700

    A push before changes

--- src/components/PostersPage/PostersPage.js
+++ src/components/PostersPage/PostersPage.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
+// import ContentSlider from './ContentSlider/ContentSlider';
 import ItemPage from '../ItemPage/ItemPage';
 
 class PostersPage extends Component {
+
     render () {
         return (
             <div className="posters-page">
                 <div className="posters-page-container">
                     <div>The Posters Page</div>
+
+                    {/* <ContentSlider /> */}
 
                     <Route path="/posters/:name" component={ ItemPage } />
 
